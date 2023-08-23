990f2092e8fd7f39cc49189083f1920d61fb6d26
alyaa999 <alyaamamoon999@gmail.com>
2023-08-23 05:56:18 +0300

    final part of js assigment

--- Day6/point1.js
+++ Day6/point1.js
@@ -5,10 +5,32 @@ var x = prompt("Enter your name:");
 
 document.write("<h1>Welcome " + x + "!</h1>");
 
-function sum ( )
-{
-     let  num1 = parseInt(prompt("first number :"));
-     let  num2 = parseInt(prompt("second number :"));
-     let res = num1+num2 ;
-      document.getElementById("res").innerHTML = "sum: " + res;
+function sum() {
+  let num1 = parseInt(prompt("first number :"));
+  let num2 = parseInt(prompt("second number :"));
+  let res = num1 + num2;
+  document.getElementById("res").innerHTML = "sum: " + res;
+}
+function Temperature(t) {
+  let result = t >= 30 ? "HOT" : "Cold";
+  console.log(result);
+}
+
+Temperature(4);
+
+function check(faculty) {
+  switch (faculty) {
+    case "FCI":
+      console.log("You're eligible for Programing tracks");
+      break;
+    case "Engineering":
+      console.log("You're eligible for Network and Embedded tracks");
+      break;
+    case "Commerce":
+      console.log("You're eligible for ERP and Social media tracks");
+      break;
+    default:
+      console.log("You're eligible for SW fundamentals track");
+      break;
+  }
 }
